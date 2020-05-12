4bf69b039283231005c005e959d8e47ea4c6c9de
Daniel J Cash <60990838+dcashdallas@users.noreply.github.com>
2020-05-12 17:39:06 -0500

    Add files via upload
    working code as of 5:30 05/12/20

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,10 +23,10 @@ function sConsole() {
             $("#results").append("<br>" + "<strong>Stock Name: </strong>" + response.quoteResponse.result[0].longName + "<br>");
             $("#results").append("<br>" + "<strong>Symbol: </strong>" + response.quoteResponse.result[0].symbol + "<br>");
             $("#results").append("<br>" + "<strong>Price: $</strong>" + response.quoteResponse.result[0].regularMarketPrice + "<br>");
-            
+
         })
 
-    }) 
+    })
 
 }
 
@@ -34,37 +34,58 @@ function Console() {
     var data = document.getElementById("stockInput");
     console.log(stockInput.value); // data is the element, and we want its value
 
-let settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://bloomberg-market-and-financial-news.p.rapidapi.com/stock/get-statistics?id=" + stockInput.value + "%253Aus",
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-host": "bloomberg-market-and-financial-news.p.rapidapi.com",
-        "x-rapidapi-key": "dbfbc65486msheb30cfc9fd5f9b7p1920c1jsn7932b76c94ef"
+    let settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://bloomberg-market-and-financial-news.p.rapidapi.com/stock/get-statistics?id=" + stockInput.value + "%253Aus",
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "bloomberg-market-and-financial-news.p.rapidapi.com",
+            "x-rapidapi-key": "dbfbc65486msheb30cfc9fd5f9b7p1920c1jsn7932b76c94ef"
+        }
     }
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+
+        $(document).ready(function () {
+            $("#results2").append("<h5><strong>Latest Financials:</strong></h5>");
+            $("#results2").append("<br>" + "<strong>Price / Earnings Ratio: </strong>" + response.result[0].table[0].value + "<br>");
+            $("#results2").append("<br>" + "<strong>Earnings Per Share: </strong>" + response.result[0].table[3].value + "<br>");
+            $("#results2").append("<br>" + "<strong>Market Capitalization: </strong>" + response.result[0].table[6].value + "<br>");
+            $("#results2").append("<br>" + "<strong>Shares Outstanding: </strong>" + response.result[0].table[7].value + "<br>");
+            $("#results2").append("<br>" + "<strong>Price / Book Ratio: </strong>" + response.result[0].table[8].value + "<br>");
+            $("#results2").append("<br>" + "<strong>Price / Sales Ratio: </strong>" + response.result[0].table[9].value + "<br>");
+            $("#results2").append("<br>" + "<strong>Dividend Indicated Gross Yield: </strong>" + response.result[0].table[10].value + "<br>");
+            $("#results2").append("<br>" + "<strong>Average Volume: </strong>" + response.result[0].table[14].value + "<br>");
+
+        })
+        // resets the page when submit/enter is pressed, appends results for new user input
+        $("#submit").click(function () {
+            $("#results").empty(),
+                $("#results2").empty();
+        })
+    })
 }
 
-$.ajax(settings).done(function (response) {
-    console.log(response);
+// function for the stock ticker 3rd party API
+(function (f, i, n, t, e, c, h) {
+    f['IntrinioWidgetsObject'] = e; f[e] = f[e] || function () {
+        (f[e].q = f[e].q || []).push(arguments)
+    }, f[e].l = 1 * new Date(); c = i.createElement(n),
+        h = i.getElementsByTagName(n)[0]; c.async = 1; c.src = t; h.parentNode.insertBefore(c, h)
+})(window, document, 'script', 'https://widget-api-cdn.intrinio.com/intrinio-widgets.min.js', 'iw');
+iw('initialize', { userId: '2095ee9f024bec25b2c782d4fe019433bdd1271613906cd7b5125229f23d654b' });
 
-    $(document).ready(function () {
-        $("#results2").append("<h5><strong>Latest Financials:</strong></h5>");
-        $("#results2").append("<br>" + "<strong>Price / Earnings Ratio: </strong>" + response.result[0].table[0].value + "<br>");
-        $("#results2").append("<br>" + "<strong>Earnings Per Share: </strong>" + response.result[0].table[3].value + "<br>");
-        $("#results2").append("<br>" + "<strong>Market Capitalization: </strong>" + response.result[0].table[6].value + "<br>");
-        $("#results2").append("<br>" + "<strong>Shares Outstanding: </strong>" + response.result[0].table[7].value + "<br>");
-        $("#results2").append("<br>" + "<strong>Price / Book Ratio: </strong>" + response.result[0].table[8].value + "<br>");
-        $("#results2").append("<br>" + "<strong>Price / Sales Ratio: </strong>" + response.result[0].table[9].value + "<br>");
-        $("#results2").append("<br>" + "<strong>Dividend Indicated Gross Yield: </strong>" + response.result[0].table[10].value + "<br>");
-        $("#results2").append("<br>" + "<strong>Average Volume: </strong>" + response.result[0].table[14].value + "<br>");
-        
-    })
-// resets the page when submit/enter is pressed, appends results for new user input
-$("#submit").click(function () {
-    $("#results").empty(),
-    $("#results2").empty();
-    })
-})
-}
+
+
+// function for modal 
+$(document).ready(function () {
+    $('.modal').modal();
+
+});
+
+
+
+
 
